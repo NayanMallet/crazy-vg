c5a1540f91482693448252e286ba25c15fe8fb20
Kylian Mallet <Kylian-Mallet@hotmail.fr>
2024-04-23 22:17:31 +0200

    🖼️ views(admin_menu.tsx): Changing component to dynamic display of menu items

--- resources/views/components/admin_menu.tsx
+++ resources/views/components/admin_menu.tsx
@@ -1,77 +1,47 @@
-// import { route } from "#start/view";
+import { Cog } from "#components/icons";
 
-type Props = {
-    appIcon?: string;
+interface Props {
+    settingsUrl: string;
+    items: Array<{
+        name: string,
+        route: string,
+        icon?: Function,
+        active?: boolean
+    }>;
 }
 
-export function AdminMenu(props: Props = {}) {
-    const { appIcon } = props;
+// TODO : Add gap between menus items
 
-    const iconStyle = `background-image: url('${appIcon}'); background-size: contain; background-position: center; background-repeat: no-repeat;`
+export function AdminMenu(props: Props) {
+    const { items, settingsUrl } = props;
 
     return (
         <main class="flex w-full h-full flex-col items-center justify-between bg-base-200">
-            <div class="flex flex-row items-center justify-center h-1/6 w-full" style={iconStyle}>
+            <div class="flex flex-row items-center justify-center h-1/6 w-full">
                 {/* Admin menu app icon */}
             </div>
 
-            <div class="flex w-full h-full flex-col items-center justify-start gap-2">
+            <div class="flex w-full h-full flex-col items-center justify-start">
                 <ul class="menu w-full rounded-box">
-                    <li>
-                        <a class="active">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                            </svg>
-                            Menu
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                            </svg>
-                            Utilisateurs
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
-                            </svg>
-                            Commandes
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                                 stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="m9 14.25 6-6m4.5-3.493V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185ZM9.75 9h.008v.008H9.75V9Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008V13.5Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                            </svg>
-                            Reçus
-                        </a>
-                    </li>
+                    {items.map(item => {
+                        return (
+                            <li>
+                                <a class={item.active ? "active" : ""} href={item.route}>
+                                    {item.icon() ?? ""}
+                                    {item.name}
+                                </a>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
 
             <div class="flex items-start justify-start w-full">
-                <button class="btn btn-ghost w-full px-4 py-2 justify-start">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
-                    Settings
-                </button>
+                <a class="btn btn-ghost w-full px-4 py-2 justify-start">
+                    <Cog />
+                    {{ settingsUrl }}
+                </a>
             </div>
         </main>
-);
+    );
 }
