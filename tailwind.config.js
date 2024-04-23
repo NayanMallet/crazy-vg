4b6e272e0025ec19803bb7c364361c8546941c3d
NayanMallet <nayan.mallet@ynov.com>
2024-04-23 17:47:26 +0200

    💎 style(admin_menu): refactor the admin menu to make it clearer + addition of logos (svg)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,35 @@ module.exports = {
     theme: {
         extend: {}
     },
-    plugins: [require("tailwindcss-animate"), require("daisyui")]
+    plugins: [require("tailwindcss-animate"), require("daisyui")],
+    daisyui: {
+        themes: [
+            "synthwave",
+            // synthwave light version
+            {
+                synthwave_light: {
+                    primary: "#f368e0",
+                    "primary-focus": "#ff5ac3",
+                    "primary-content": "#ffffff",
+                    secondary: "#f368e0",
+                    "secondary-focus": "#ff5ac3",
+                    "secondary-content": "#ffffff",
+                    accent: "#1982c4",
+                    "accent-focus": "#055c9d",
+                    "accent-content": "#ffffff",
+                    neutral: "#2d2b55",
+                    "neutral-focus": "#232145",
+                    "neutral-content": "#ffffff",
+                    "base-100": "#ffffff",
+                    "base-200": "#f9fafb",
+                    "base-300": "#d1d5db",
+                    "base-content": "#1f2937",
+                    info: "#2094f3",
+                    success: "#009485",
+                    warning: "#ff9900",
+                    error: "#ff5724"
+                }
+            }
+        ]
+    }
 };
