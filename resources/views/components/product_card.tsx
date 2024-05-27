0770089f6774f1ff5fa4cf329e1c70f0e76194e1
NayanMallet <nayan.mallet@ynov.com>
2024-05-27 18:20:04 +0200

    🖼️ views(product-card): add product card

--- resources/views/components/product_card.tsx
+++ resources/views/components/product_card.tsx
@@ -6,21 +6,27 @@ type Props = {
 }
 
 export function ProductCard(props: Props) {
-    const { product } = props
+    const { product } = props;
+
+    const ratingPercentage = (product.averageRating / 5) * 100;
 
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={product.thumbnailUrl} alt="Shoes"/>
+            <figure>
+                <img src={product.thumbnailUrl} alt={product.name} />
             </figure>
             <div class="card-body">
-                <h2 class="card-title">
-                    {product.name}
-                    <div class="badge badge-secondary">NEW</div>
-                </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <h2 class="card-title">{product.name}</h2>
+                <p>{product.description}</p>
                 <div class="card-actions justify-end">
-                    <div class="badge badge-outline">Fashion</div>
-                    <div class="badge badge-outline">Products</div>
+                    <span class="badge badge-secondary">Price: ${product.price}</span>
+                    <div class="rating gap-1">
+                        <input type="radio" name="rating" class="mask mask-heart bg-red-400" style={{ "--tw-radio-ring-color": "transparent" }} checked={product.averageRating >= 0.5} />
+                        <input type="radio" name="rating" class="mask mask-heart bg-orange-400" style={{ "--tw-radio-ring-color": "transparent" }} checked={product.averageRating >= 1.5} />
+                        <input type="radio" name="rating" class="mask mask-heart bg-yellow-400" style={{ "--tw-radio-ring-color": "transparent" }} checked={product.averageRating >= 2.5} />
+                        <input type="radio" name="rating" class="mask mask-heart bg-lime-400" style={{ "--tw-radio-ring-color": "transparent" }} checked={product.averageRating >= 3.5} />
+                        <input type="radio" name="rating" class="mask mask-heart bg-green-400" style={{ "--tw-radio-ring-color": "transparent" }} checked={product.averageRating >= 4.5} />
+                    </div>
                 </div>
             </div>
         </div>
