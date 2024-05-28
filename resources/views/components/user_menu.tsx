ce63b3a8615f5e8548f0c43bfd136b278ca4d2a7
NotGrafik <pablo.jeanlouis@ynov.com>
2024-05-28 11:07:18 +0200

    💎(style): on profile page

--- resources/views/components/user_menu.tsx
+++ resources/views/components/user_menu.tsx
@@ -7,62 +7,78 @@ type Props = {
 export function UserMenu(props: Props) {
     const { loggedUser } = props;
 
+    let isModifying = false;
+
+
     return (
         <main class="flex flex-col items-center justify-between w-full h-full">
-            <div class="avatar">
-                <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                    <img src={loggedUser.avatarUrl ?? "https://i.pravatar.cc/500?img=32"} alt="avatar" />
+            <div class="flex flex-col gap-10 h-screen justify-center align-center">
+                <div class="avatar justify-center">
+                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={loggedUser.avatarUrl ?? "https://i.pravatar.cc/500?img=32"} alt="avatar" />
+                    </div>
+                    <div class="badge badge-primary">admin</div>
                 </div>
-                <div class="badge badge-primary">admin</div>
-            </div>
 
-            <div class="flex flex-col gap-2">
-                <label class="input input-bordered flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                         class="w-4 h-4 opacity-70">
-                        <path
-                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                        <path
-                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                    </svg>
-                    <input type="text" class="grow" placeholder={loggedUser.email} value={loggedUser.email} disabled/>
-                </label>
-                <label class="input input-bordered flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                         class="w-4 h-4 opacity-70">
-                        <path
-                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                    </svg>
-                    <input type="text" class="grow" placeholder={loggedUser.firstName ?? "Username"} value={loggedUser.firstName ?? "Username"} />
+                <div>
+                    <p class="text-center">Solde: <span class="solde">345</span>€</p>
+                </div>
+
+                <div class="flex flex-col gap-2">
+                    <label class="input input-bordered flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                            class="w-4 h-4 opacity-70">
+                            <path
+                                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                            <path
+                                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                        </svg>
+                        <input type="text" class="grow" placeholder={loggedUser.email} value={loggedUser.email} disabled />
                     </label>
-                <label class="input input-bordered flex items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                         class="w-4 h-4 opacity-70">
-                        <path fill-rule="evenodd"
-                              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                              clip-rule="evenodd" />
+                    <label class="input input-bordered flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                            class="w-4 h-4 opacity-70">
+                            <path
+                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                        </svg>
+                        <input type="text" class="grow" placeholder={loggedUser.firstName ?? "Username"} value={loggedUser.firstName ?? "Username"} disabled />
+                        </label>
+                    <label class="input input-bordered flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                            class="w-4 h-4 opacity-70">
+                            <path fill-rule="evenodd"
+                                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <input type="password" class="grow" placeholder={loggedUser.password ?? "password"} value={loggedUser.password ?? "password"} disabled />
+                    </label>
+                    <label class="hidden input input-bordered items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                            class="w-4 h-4 opacity-70">
+                            <path fill-rule="evenodd"
+                                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <input type="password" class="grow" placeholder={loggedUser.password ?? "password"} value={loggedUser.password ?? "password"} disabled />
+                    </label>
+                </div>
+                {isModifying ? (
+                    <div class="flex flex-col gap-2">
+                        <button class="btn btn-primary">Save</button>
+                        <button x-data="{ isModifying: false }" class="btn" onclick="document.querySelectorAll('input').forEach(input => input.addAttribute('disabled'));">Cancel</button>
+                    </div>
+                ) : 
+                // remove disabled attribute to enable modification
+                <button class="btn" onclick="document.querySelectorAll('input').forEach(input => input.removeAttribute('disabled'));">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                     </svg>
-                    <input type="password" class="grow" placeholder={loggedUser.password ?? "password"} value={loggedUser.password ?? "password"} />
-                </label>
-                <label class="hidden input input-bordered items-center gap-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                         class="w-4 h-4 opacity-70">
-                        <path fill-rule="evenodd"
-                              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                              clip-rule="evenodd" />
-                    </svg>
-                    <input type="password" class="grow" placeholder={loggedUser.password ?? "password"} value={loggedUser.password ?? "password"} />
-                </label>
+                    Modify
+                </button>
+                }
             </div>
-
-            <button class="btn">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                </svg>
-                Modify
-            </button>
         </main>
     );
 }
