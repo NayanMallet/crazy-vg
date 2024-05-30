864faa392af5e9e2c555d6e0d2c5f8f5fffa47cd
NotGrafik <pablo.jeanlouis@ynov.com>
2024-05-30 22:08:30 +0200

    🖼️(views): fix navbar on users

--- resources/views/pages/admin/users.tsx
+++ resources/views/pages/admin/users.tsx
@@ -1,12 +1,38 @@
 import { AdminHeader } from "#components/admin_header";
 import { UsersList } from "#components/users_list";
 import { AdminLayout } from "#layouts/admin_layout";
+import { AdminMenu } from "#components/admin_menu";
+import { Home, User as UserIcon, Recipe, Wallet } from "#components/icons";
 
 import User from "#models/user";
 
 interface Props {
     user: User,
 }
+
+const routes = [
+    {
+        name: "Dashboard",
+        route: "/admin",
+        icon: Home,
+    },
+    {
+        name: "Utilisateurs",
+        route: "/admin/users",
+        icon: UserIcon,
+        active: true
+    },
+    {
+        name: "Commandes",
+        route: "/admin/orders",
+        icon: Recipe
+    },
+    {
+        name: "Produits",
+        route: "/admin/products",
+        icon: Wallet,
+    }
+];
 
 export async function Users(props: Props) {
     const { user } = props;
@@ -15,7 +41,7 @@ export async function Users(props: Props) {
     return (
         <AdminLayout
             title={'Users'}
-            navigation={<div>Users</div>}
+            navigation={<AdminMenu items={routes} settingsUrl={"admin/users"}/>}
             header={<AdminHeader pageTitle={"Hey " + user.firstName + " !"} loggedUser={user} />}
             main={
                 <UsersList users={users} />
