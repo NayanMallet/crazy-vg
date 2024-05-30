3701da5233833d9f008e671d6903249e5bdbcaa5
NotGrafik <pablo.jeanlouis@ynov.com>
2024-05-30 19:08:01 +0200

    🖼️(views): fix request

--- resources/views/components/products_list.tsx
+++ resources/views/components/products_list.tsx
@@ -24,26 +24,24 @@ export function ProductsList(props: Props) {
                 </thead>
 
                 <tbody>
-                {products.map((product) => (
-                    <ListItem
-                        id={product.id}
-                        checkbox={false}
-                        name={product.name}
-                        property={product.price.toString()}
-                        badges={[]}
-                        thumbnailUrl={product.thumbnailUrl ?? undefined}
-                        otherData={[product.averageRating.toString(), product.activationCode]}
-                        actions={<div class="flex gap-2">
-                            <button class="btn btn-sm btn-square btn-outline p-2">
-                                <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
-                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"></path>
-                                </svg>
-                            </button>
-                        </div>}
-                    />
-                ))}
+                    {products.map(product => (
+                        <ListItem
+                            id={product.id}
+                            thumbnailUrl={product.thumbnailUrl}
+                            name={product.name}
+                            property={product.price.toString()}
+                            otherData={[product.averageRating.toString(), product.activationCode]}
+                            actions={
+                                <button class="btn btn-sm btn-square btn-outline p-2">
+                                    <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"></path>
+                                    </svg>
+                                </button>
+                            }
+                        />
+                    ))}
                 </tbody>
             </table>
         </div>
--- resources/views/pages/admin/admin_products.tsx
+++ resources/views/pages/admin/admin_products.tsx
@@ -1,17 +1,47 @@
 import { AdminHeader } from "#components/admin_header";
+import { AdminMenu } from "#components/admin_menu";
 import { AdminLayout } from "#layouts/admin_layout";
 import { ProductsList } from "#components/products_list";
 import User from "#models/user";
 import Product from "#models/product"; // Importer le modèle Product
+import { Home, User as UserIcon, Recipe, Wallet } from "#components/icons";
+
+
 
 interface Props {
-    user: User,
+    product: Product,
+    user: User
 }
 
 export async function AdminProducts(props: Props) {
-    const { user } = props;
+    const { product, user } = props;
+    const products = await Product.all(); // Récupérer tous les produits
 
-    // Fonction pour récupérer les produits
+    const routes = [
+        {
+            name: "Dashboard",
+            route: "/admin/dashboard",
+            icon: Home,
+        },
+        {
+            name: "Utilisateurs",
+            route: "/admin/users",
+            icon: UserIcon
+        },
+        {
+            name: "Commandes",
+            route: "/admin/orders",
+            icon: Recipe
+        },
+        {
+            name: "Produits",
+            route: "/admin/products",
+            icon: Wallet,
+            active: true
+        }
+    ];
+
+    /* // Fonction pour récupérer les produits
     const fetchProducts = async () => {
         try {
             const products = await Product.all();
@@ -23,12 +53,12 @@ export async function AdminProducts(props: Props) {
     };
 
     // Appeler la fonction pour récupérer les produits
-    const products = await fetchProducts();
+    const products = await fetchProducts(); */
 
     return (
         <AdminLayout
             title={'Products'}
-            navigation={<div>Products</div>}
+            navigation={<AdminMenu items={routes} settingsUrl={"admin/products"}/>}
             header={<AdminHeader pageTitle={"Hey " + user.firstName + " !"} loggedUser={user} />}
             main={<ProductsList products={products} />}
         />
