6ed1b5730aa7e4963055c2c17b821a40ed47f07e
NayanMallet <nayan.mallet@ynov.com>
2024-05-31 00:38:48 +0200

    🛣️ adding controllers for edit / update / delete users

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,29 +1,47 @@
 import type { HttpContext } from "@adonisjs/core/http";
+import User from "#models/user";
 
 export default class UsersController {
     async index({}: HttpContext) {
     }
 
-    async create({}: HttpContext) {
+    public async create({view}: HttpContext) {
+        return view.render('users.create')
     }
 
 
-    async store({ request }: HttpContext) {
+    async store({ request, response }: HttpContext) {
+        const data = request.only(['email', 'password'])
+        const user = await User.create(data)
+        return response.redirect('/')
     }
 
 
-    async show({ params }: HttpContext) {
+
+    async edit({ params, view }: HttpContext) {
+        const user = await User.find(params.id)
+        return view.render('users.edit', { user })
     }
 
 
-    async edit({ params }: HttpContext) {
+    async update({ params, request, response }: HttpContext) {
+        const user = await User.find(params.id)
+        const data = request.only(['email', 'password'])
+        if (!user) {
+            return response.notFound('User not found')
+        }
+        user.merge(data)
+        await user.save()
+        return response.redirect('/admin/users')
     }
 
 
-    async update({ params, request }: HttpContext) {
-    }
-
-
-    async destroy({ params }: HttpContext) {
+    async destroy({ params, response }: HttpContext) {
+        const user = await User.find(params.id)
+        if (!user) {
+            return response.notFound('User not found')
+        }
+        await user.delete()
+        return response.redirect('/admin/users')
     }
 }
